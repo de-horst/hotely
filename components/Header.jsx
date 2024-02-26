a26fb3db5fe521928432cad68d38ed618c8427a3
Denise Horst <denisef.horst@gmail.com>
2024-02-26 14:36:07 -0300

    02 Header + Nav + MobileNav + Socials

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,40 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import MobileNav from "./MobileNav";
+import Nav from "./Nav";
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="absolute w-full max-w-[1440px] py-8 xl:py-10">
+      <div className="container mx-auto">
+        {/* Logo, nav (desktop), btn */}
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <Link href="/">
+            <h1 className="text-2xl font-bold text-white">Hotely</h1>
+          </Link>
 
-export default Header
+          {/* Nav & btn */}
+          <div className="hidden xl:flex">
+            <div className="flex items-center gap-x-12">
+              <Nav
+                containerStyles="flex items-center justify-between"
+                listStyles="flex gap-x-12 text-white"
+              />
+              <Button variant="outline" className="px-10 h-[58px]">
+                Login
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        {/* Mobile Nav */}
+        <div className="xl:hidden absolute right-8 top-8">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
